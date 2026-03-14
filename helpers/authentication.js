a007edf6604727b39b65b3a69fbe361ab7287aa6
victorFeracin <victorhugo.feracin@gmail.com>
2026-03-13 22:22:00 -0300

    Adding fixtures for login and transfer body

--- helpers/authentication.js
+++ helpers/authentication.js
@@ -1,14 +1,14 @@
 import request from 'supertest';
 import 'dotenv/config';
+import postLogin from '../fixtures/postLogin.json' assert { type: "json" };
 
 const getToken = async (username, password) => {
+  const bodyLogin = { ...postLogin };
+  
   const response = await request(process.env.BASE_URL)
     .post('/login')
     .set('Content-Type', 'application/json')
-    .send({
-      'username': username,
-      'senha': password
-    });
+    .send(bodyLogin);
 
   return response.body.token;
 }
--- test/login.test.js
+++ test/login.test.js
@@ -1,17 +1,17 @@
 import request from 'supertest';
 import { expect } from 'chai';
 import 'dotenv/config';
+import postLogin from '../fixtures/postLogin.json' assert { type: "json" };
 
 describe('Login', () => {
   describe('POST /login', () =>{
     it('Should return status 200 with a string token when inserting valid credentials', async () => {
+      const bodyLogin = { ...postLogin };
+
       const response = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
-        .send({
-          'username': 'julio.lima',
-          'senha': '123456'
-        });
+        .send(bodyLogin);
 
       expect(response.status).to.equal(200);
       expect(response.body.token).to.be.a('string');
